f1da358a31032deeef9c2fbdcfae6eafc8afb8a2
Anton Zavyalov <zavyalov.anton@huawei.com>
2021-01-14 16:59:23 +0700

    Написал Команду

--- src/main/kotlin/main/Main.kt
+++ src/main/kotlin/main/Main.kt
@@ -1,30 +1,22 @@
 package main
 
+import behavioral.command.Dataflow
+import behavioral.command.Node
 import behavioral.indirection.ModelNode
 import behavioral.indirection.NodeBinding
 import behavioral.indirection.ViewNode
-import common.data.DummyAudioData
 import common.logging.Logger
 import common.logging.writers.FileLogWriter
-import common.utils.Either
-import creational.builder.NodeBuilder
-import creational.object_pool.FontPool
-import creational.object_pool.PoolFontData
-import structural.bridge.AsioProcessorImpl
-import structural.bridge.NativeProcessor
-import structural.bridge.NativeVendorException
-import structural.bridge.TraceableNativeProcessor
-import structural.facade.Dataflow
-import structural.facade.Node
-import structural.flyweight.Font
-import structural.flyweight.FontDataFactory
-import structural.flyweight.Paint
-import structural.flyweight.TypeFace
 
 fun main() {
     Logger.wrap(FileLogWriter("indirection")) {
         entitle("Indirection") {
             indirection()
+        }
+    }
+    Logger.wrap(FileLogWriter("command")) {
+        entitle("Command") {
+            command()
         }
     }
 }
@@ -44,6 +36,34 @@ fun indirection() {
     model.setParameter(0, 0.45f)
     model.setParameter(1, 0.75f)
     model.setParameter(2, 0.9f)
+}
+
+fun command() {
+    val dataFlow = Dataflow<String>()
+
+    val input = Node<String>(0, 1) {
+        "Input".also { println(it) }
+    }.also { dataFlow.add(it) }
+
+    val output = Node<String>(1, 0) {
+        "Output".also { println(it) }
+    }.also { dataFlow.add(it) }
+
+    val distortion = Node<String>(1, 1) {
+        "Distortion".also { println(it) }
+    }.also { dataFlow.add(it) }
+
+    val delay = Node<String>(1, 1) {
+        "Delay".also { println(it) }
+    }.also { dataFlow.add(it) }
+
+    dataFlow.connect { connector ->
+        connector(input, 0, distortion, 0)
+        connector(distortion, 0, delay, 0)
+        connector(delay, 0, output, 0)
+    }
+
+    dataFlow.makePipeline()(listOf())
 }
 
 fun entitle(name: String, block: () -> Unit) {
